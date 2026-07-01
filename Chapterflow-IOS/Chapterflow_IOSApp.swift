233eebaa5628e58cce52e435be20db069985fe7e
WillSoltani <Soltani.willx@gmail.com>
2026-06-30 22:46:45 -0300

    Initial Commit

---
+++ Chapterflow-IOS/Chapterflow_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chapterflow_IOSApp.swift
+//  Chapterflow-IOS
+//
+//  Created by Will Soltani on 2026-06-30.
+//
+
+import SwiftUI
+
+@main
+struct Chapterflow_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chapterflow-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chapterflow-IOS
+//
+//  Created by Will Soltani on 2026-06-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
